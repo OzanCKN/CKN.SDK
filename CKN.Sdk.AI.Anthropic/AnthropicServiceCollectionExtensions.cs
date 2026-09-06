97bb90e0d7e78cd971f95b6d2e561dc47f55a74e
ozanckn <c.k.n@msn.com>
2026-09-06 05:35:06 +0300

    feat: implement Anthropic native provider

---
+++ CKN.Sdk.AI.Anthropic/AnthropicServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+using System;
+using Anthropic.SDK;
+using CKN.Sdk.AI.Anthropic;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class AnthropicServiceCollectionExtensions
+{
+    /// <summary>
+    /// Configures the DI container to use Anthropic's native client.
+    /// </summary>
+    public static IServiceCollection AddCknAnthropic(this IServiceCollection services, Action<AnthropicOptions> configureOptions)
+    {
+        var options = new AnthropicOptions();
+        configureOptions(options);
+
+        services.Configure(configureOptions);
+
+        services.TryAddSingleton<AnthropicClient>(sp =>
+        {
+            return new AnthropicClient(new APIAuthentication(options.ApiKey));
+        });
+
+        return services;
+    }
+}
